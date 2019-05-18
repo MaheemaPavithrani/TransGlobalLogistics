d7fd79985030ff33a578f6026a5d73e30ae4e4da
Tharinda Dilshan Piyadasa <tharindad7@gmail.com>
2019-05-19 02:39:58 +0530

    admin | ui

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -17,13 +17,23 @@
     </title>
 </head>
 <body>
-    <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0" id="nav">
+    <nav class="navbar navbar-dark navbar-toggleable-xl sticky-top bg-dark flex-md-nowrap p-0" id="nav">
       <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#"><h3>Trans Global Logistics</h3></a>
       <!-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> -->
+     
       <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-          <a class="nav-link" href="#">Sign out</a>
-        </li>
+        <div class="row">
+          <li class="nav-item text-nowrap" style="padding-right:10px">
+            <a class="nav-link" href="#"><i class="fa fa-bell fa-fw"></i> Notifications</a>
+          </li>
+          <li class="nav-item text-nowrap" style="padding-right:10px">
+            <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown"><i class="fa fa-user fa-fw"></i> Admin</a>
+            <div class="dropdown-menu dropdown-menu-right" style="right: 0; left: auto;">
+                <a class="dropdown-item" href="">Profile</a>
+                <a class="dropdown-item" href="">Logout</a>
+            </div>
+          </li>
+        </div> 
       </ul>
     </nav>
 
@@ -33,28 +43,28 @@
           <div class="sidebar-sticky">
             <ul class="nav flex-column">
       
-                <a href="<?php echo base_url('admin/index');?>" class="list-group-item list-group-item-light" ><h5>DASHBOARD</h5></a>
+                <a href="<?php echo base_url('admin/index');?>" class="list-group-item list-group-item-light" style="color:black" ><h5>DASHBOARD</h5></a>
 
-                <a href="#customer" class="list-group-item list-group-item-light" data-toggle="collapse" ><h5><i class="fa fa-users fa-fw fa-fw" aria-hidden="true"></i>CUSTOMERS</h5></a>
+                <a href="#customer" class="list-group-item list-group-item-light" data-toggle="collapse" style="color:black" ><h5><i class="fa fa-users fa-fw fa-fw" aria-hidden="true"></i>  CUSTOMERS</h5></a>
                 <div class="collapse" id="customer">
                   <a href="<?php echo base_url('admin/get_customers');?>" class="list-group-item">View All</a>
                   <a href="<?php echo base_url('admin/register_customer');?>" class="list-group-item">Add customer</a>
                 </div>
             
-                <a href="#hire" class="list-group-item list-group-item-light" data-toggle="collapse" ><h5><i class="fa fa-file fa-fw"></i>HIRES</h5></a>
+                <a href="#hire" class="list-group-item list-group-item-light" data-toggle="collapse" style="color:black"><h5><i class="fa fa-file fa-fw"></i>  HIRES</h5></a>
                 <div class="collapse" id="hire">
                   <a href="" class="list-group-item">Ongoing</a>
                   <a href="" class="list-group-item">Imports</a>
                   <a href="" class="list-group-item">Exports</a>
                 </div>
 
-                <a href="#drivers" class="list-group-item list-group-item-light" data-toggle="collapse" ><h5><i class="fa fa-address-card fa-fw"></i>DRIVERS</h5></a>
+                <a href="#drivers" class="list-group-item list-group-item-light" data-toggle="collapse" style="color:black" ><h5><i class="fa fa-address-card fa-fw"></i>  DRIVERS</h5></a>
                 <div class="collapse" id="drivers">
                   <a href="<?php echo base_url('admin/get_drivers');?>" class="list-group-item">View All</a>
                   <a href="<?php echo base_url('admin/register_driver');?>" class="list-group-item">Add Driver</a>
                 </div>
 
-                <a href="#vehicle" class="list-group-item list-group-item-light" data-toggle="collapse" ><h5><i class="fa fa-truck fa-fw"></i>VEHICLES</h5></a>
+                <a href="#vehicle" class="list-group-item list-group-item-light" data-toggle="collapse" style="color:black" ><h5><i class="fa fa-truck fa-fw"></i>  VEHICLES</h5></a>
                 <div class="collapse" id="vehicle">
                   <a href="" class="list-group-item">View All</a>
                   <a href="" class="list-group-item">Add Vehicle</a>
@@ -96,7 +106,6 @@
             </ul> -->
           </div>
         </nav>
- 
 
         
     
