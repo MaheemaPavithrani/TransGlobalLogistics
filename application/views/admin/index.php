21bcddc1b2d8ee713bbc9f99b1fb712595abb1a3
Tharinda Dilshan Piyadasa <tharindad7@gmail.com>
2019-06-18 18:00:42 +0530

    Footer

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -9,33 +9,34 @@
   <div class="col-6">col-6</div>
   <div class="col">col-3</div>
 </div> -->
-    <div class="col-xs-10 col-sm-10">
+    <div class="col-lg-10 col-lg-10">
       <div style="padding-top:10px">
         <h2>This Month</h2>
       </div>
       <div class="row">
-        <div class="col-md-4">
+        <div class="col-lg-4">
           <div class="card-counter primary">
             <i class="fa fa-clock"></i>
             <span class="count-numbers">12</span>
             <span class="count-name">Pending Hires</span>
           </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-lg-4">
           <div class="card-counter danger">
             <i class="fa fa-truck"></i>
             <span class="count-numbers">23</span>
             <span class="count-name">Hires</span>
           </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-lg-4">
           <div class="card-counter info">
             <i class="fa fa-users"></i>
             <span class="count-numbers">35</span>
             <span class="count-name">New Customers</span>
           </div>
         </div>
-      </div>  
+      </div> 
+      
     </div>
     
 
